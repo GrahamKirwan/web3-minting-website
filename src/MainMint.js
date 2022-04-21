de1f6e30d8792c6fa79cce3405da384524bdd513
Graham Kirwan <graham.kirwan4@gmail.com>
2022-04-20 22:52:34 -0400

    frontend updates

--- src/MainMint.js
+++ src/MainMint.js
@@ -49,7 +49,7 @@ export default function MainMint({ accounts, setAccounts }) {
         <Flex justify="center" align="center" height="100vh" paddingBottom="150px">
             <Box width="520px">
                 <div>
-            <Text fontSize="48px" textShadow="0 5px #000000">RoboPunks</Text>
+            <Text fontSize="48px" textShadow="0 5px #000000">RoboPunksNFT</Text>
             <Text fontSize="30px" letterSpacing="-5.5%" fontFamily="VT323" textShadow="0 2px 2px #000000">A collection of 1000 RoboPunks living on the Ethereum blockchain. Mint below!</Text>
             </div>
             
